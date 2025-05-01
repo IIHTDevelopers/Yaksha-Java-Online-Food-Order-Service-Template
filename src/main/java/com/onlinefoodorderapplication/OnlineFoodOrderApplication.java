ae838c462ce9b95b738de960912b6491de5b07a1
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-01 11:07:38 +0530

    Update OnlineFoodOrderApplication.java

--- src/main/java/com/onlinefoodorderapplication/OnlineFoodOrderApplication.java
+++ src/main/java/com/onlinefoodorderapplication/OnlineFoodOrderApplication.java
@@ -2,12 +2,18 @@ package com.onlinefoodorderapplication;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
 import org.jboss.jandex.Main;
 
+import com.onlinefoodorderapplication.model.Dish;
+import com.onlinefoodorderapplication.model.Restaurant;
 import com.onlinefoodorderapplication.repository.DishDAO;
 import com.onlinefoodorderapplication.repository.DishDAOImpl;
 import com.onlinefoodorderapplication.repository.RestaurantDAO;
@@ -90,12 +96,32 @@ public class OnlineFoodOrderApplication {
 
 	}
 
-	public static void createDatabaseAndTablesIfNotExists(String url, String username, String password) {
+	public static void createDatabaseAndTablesIfNotExists(String url, String username, String password)
+			throws SQLException {
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			String createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS food_order_service";
+			String createRestaurantTableQuery = "CREATE TABLE IF NOT EXISTS restaurant ("
+					+ "id INT AUTO_INCREMENT PRIMARY KEY," + "name VARCHAR(20) NOT NULL,"
+					+ "address VARCHAR(10) NOT NULL" + ")";
+			String createDishTableQuery = "CREATE TABLE IF NOT EXISTS dish (" + "id INT AUTO_INCREMENT PRIMARY KEY,"
+					+ "name VARCHAR(10) NOT NULL," + "ingredients VARCHAR(100) NOT NULL,"
+					+ "restaurant_id INT NOT NULL," + "FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)" + ")";
 
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(createDatabaseQuery);
+			statement.executeUpdate("USE food_order_service");
+			statement.executeUpdate(createRestaurantTableQuery);
+			statement.executeUpdate(createDishTableQuery);
+		}
 	}
 
 	public static void createDatabaseIfNotExists(String url, String username, String password) throws SQLException {
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			String createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS food_order_service";
 
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(createDatabaseQuery);
+		}
 	}
 
 	private static void searchRestaurantsByName() {
